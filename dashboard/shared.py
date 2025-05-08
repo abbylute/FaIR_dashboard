03c2374be7132309d93193b71f6fdf0e578d9cda
Abby Lute <aby.lute@gmail.com>
2025-05-08 15:12:21 -0700

    updated to use files with combined scenarios

--- dashboard/shared.py
+++ dashboard/shared.py
@@ -5,6 +5,14 @@ import pandas as pd
 app_dir = Path(__file__).parent
 #df = pd.read_csv(app_dir / "fair_warming.csv")
 
-fair_dir = "/Users/abbylute/Documents/FaIR/outputs/AL_V5/csv/"
-df = pd.read_csv(fair_dir + "fair_warming.csv")
-dfmed = pd.read_csv(fair_dir + "fair_warming_scenario_medians.csv")
+#fair_dir = "/Users/abbylute/Documents/FaIR/outputs/AL_V5/csv/"
+#df = pd.read_csv(fair_dir + "fair_warming.csv")
+#dfmed = pd.read_csv(fair_dir + "fair_warming_scenario_medians.csv")
+
+#cmip7_dir = "/Users/abbylute/Documents/FaIR/chrisroadmap-cmip7-scenariomip/output/csv/"
+#df_cmip7 = pd.read_csv(cmip7_dir + "fair_warming.csv")
+#dfmed_cmip7 = pd.read_csv(cmip7_dir + "fair_warming_scenario_medians.csv")
+
+data_dir =  "/Users/abbylute/Documents/FaIR/FaIR_dashboard_data/"
+df = pd.read_csv(data_dir + "fair_warming.csv")
+dfmed = pd.read_csv(data_dir + "fair_warming_scenario_medians.csv")
