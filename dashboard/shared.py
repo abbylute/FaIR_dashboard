49d2cb02611244981ffa0cc8ac6526c76b8af3a5
Abby Lute <aby.lute@gmail.com>
2025-05-27 11:20:48 -0700

    clean up import file locating

--- dashboard/shared.py
+++ dashboard/shared.py
@@ -3,17 +3,8 @@ from pathlib import Path
 import pandas as pd
 
 app_dir = Path(__file__).parent
-#df = pd.read_csv(app_dir / "fair_warming.csv")
+app_dir1 = Path(__file__).parent.parent
 
-#fair_dir = "/Users/abbylute/Documents/FaIR/outputs/AL_V5/csv/"
-#df = pd.read_csv(fair_dir + "fair_warming.csv")
-#dfmed = pd.read_csv(fair_dir + "fair_warming_scenario_medians.csv")
-
-#cmip7_dir = "/Users/abbylute/Documents/FaIR/chrisroadmap-cmip7-scenariomip/output/csv/"
-#df_cmip7 = pd.read_csv(cmip7_dir + "fair_warming.csv")
-#dfmed_cmip7 = pd.read_csv(cmip7_dir + "fair_warming_scenario_medians.csv")
-
-data_dir =  "/Users/abbylute/Documents/FaIR/FaIR_dashboard/data/"
-df = pd.read_csv(data_dir + "fair_warming.csv")
-dfmed = pd.read_csv(data_dir + "fair_warming_scenario_medians_exceedances.csv")
-dfquant = pd.read_csv(data_dir + "fair_warming_scenario_quantiles.csv")
+df = pd.read_csv(app_dir1 / "data/fair_warming.csv")
+dfmed = pd.read_csv(app_dir1 / "data/fair_warming_scenario_medians_exceedances.csv")
+dfquant = pd.read_csv(app_dir1 / "data/fair_warming_scenario_quantiles.csv")
